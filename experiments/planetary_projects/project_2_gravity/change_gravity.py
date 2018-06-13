94734a2ef9dc6953a63462fcd91a6ab89898c8fe
sit23 <stephen.i.thomson@gmail.com>
2018-06-13 16:50:07 +0100

    Seed experiment with gravity - deliberately leaving out other things they'll have to scale.

---
+++ experiments/planetary_projects/project_2_gravity/change_gravity.py
@@ -0,0 +1,130 @@
+import os
+
+import numpy as np
+
+from isca import IscaCodeBase, DiagTable, Experiment, Namelist, GFDL_BASE
+
+base_dir = os.path.dirname(os.path.realpath(__file__))
+# a CodeBase can be a directory on the computer,
+# useful for iterative development
+cb = IscaCodeBase.from_directory(GFDL_BASE)
+
+# or it can point to a specific git repo and commit id.
+# This method should ensure future, independent, reproducibility of results.
+# cb = DryCodeBase.from_repo(repo='https://github.com/isca/isca', commit='isca1.1')
+
+# compilation depends on computer specific settings.  The $GFDL_ENV
+# environment variable is used to determine which `$GFDL_BASE/src/extra/env` file
+# is used to load the correct compilers.  The env file is always loaded from
+# $GFDL_BASE and not the checked out git repo.
+
+# create an Experiment object to handle the configuration of model parameters
+# and output diagnostics
+
+inputfiles=[]
+
+#Tell model how to write diagnostics
+diag = DiagTable()
+diag.add_file('atmos_monthly', 30, 'days', time_units='days')
+
+#Tell model which diagnostics to write
+diag.add_field('dynamics', 'ps', time_avg=True)
+diag.add_field('dynamics', 'bk')
+diag.add_field('dynamics', 'pk')
+diag.add_field('dynamics', 'sphum', time_avg=True)
+diag.add_field('dynamics', 'ucomp', time_avg=True)
+diag.add_field('dynamics', 'vcomp', time_avg=True)
+diag.add_field('dynamics', 'temp', time_avg=True)
+diag.add_field('dynamics', 'vor', time_avg=True)
+diag.add_field('dynamics', 'div', time_avg=True)
+diag.add_field('dynamics', 'height', time_avg=True)
+
+#Define values for the 'core' namelist
+namelist = Namelist({
+    'main_nml': {
+        'dt_atmos': 600,
+        'days': 30,
+        'calendar': 'thirty_day',
+        'current_date': [2000,1,1,0,0,0]
+    },
+
+    'atmosphere_nml': {
+        'idealized_moist_model': False  # False for Newtonian Cooling.  True for Isca/Frierson
+    },
+
+    'spectral_dynamics_nml': {
+        'damping_order'           : 4,                      # default: 2
+        'water_correction_limit'  : 200.e2,                 # default: 0
+        'reference_sea_level_press': 1.0e5,                  # default: 101325
+        'valid_range_t'           : [100., 800.],           # default: (100, 500)
+        'initial_sphum'           : 0.0,                  # default: 0
+        'vert_coord_option'       : 'uneven_sigma',         # default: 'even_sigma'
+        'scale_heights': 6.0,
+        'exponent': 7.5,
+        'surf_res': 0.5
+    },
+
+    # configure the relaxation profile
+    'hs_forcing_nml': {
+        't_zero': 315.,    # temperature at reference pressure at equator (default 315K)
+        't_strat': 200.,   # stratosphere temperature (default 200K)
+        'delh': 60.,       # equator-pole temp gradient (default 60K)
+        'delv': 10.,       # lapse rate (default 10K)
+        'eps': 0.,         # stratospheric latitudinal variation (default 0K)
+        'sigma_b': 0.7,    # boundary layer friction height (default p/ps = sigma = 0.7)
+
+        # negative sign is a flag indicating that the units are days
+        'ka':   -40.,      # Constant Newtonian cooling timescale (default 40 days)
+        'ks':    -4.,      # Boundary layer dependent cooling timescale (default 4 days)
+        'kf':   -1.,       # BL momentum frictional timescale (default 1 days)
+
+        'do_conserve_energy':   True,  # convert dissipated momentum into heat (default True)
+    },
+
+    'diag_manager_nml': {
+        'mix_snapshot_average_fields': False
+    },
+
+    'fms_nml': {
+        'domains_stack_size': 600000                        # default: 0
+    },
+
+    'fms_io_nml': {
+        'threading_write': 'single',                         # default: multi
+        'fileset_write': 'single',                           # default: multi
+    }, 
+    
+    'constants_nml': {
+        'grav':9.80,
+    },
+    
+})
+
+
+#Lets do a run!
+if __name__=="__main__":
+
+    NCORES=16
+
+    earth_grav = 9.80
+
+    grav_earth_multiple = [1, 2]
+
+    for grav_scale in make_symmetric_values_list:
+
+        cb.compile()
+        exp = Experiment('project_2_grav_earth_multiple_'+str(grav_scale), codebase=cb)
+        exp.clear_rundir()
+
+        exp.diag_table = diag
+        exp.inputfiles = inputfiles
+
+        exp.namelist = namelist.copy()
+        
+        #Note that only gravity is changed here, but what else should be kept constant in order to isolate the effect of gravity alone?
+        
+        exp.namelist['constants_nml']['grav'] = earth_grav * grav_scale
+
+        exp.run(1, use_restart=False, num_cores=NCORES)
+        for i in range(2,121):
+            exp.run(i, num_cores=NCORES)
