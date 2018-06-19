3fab5a3592e9d2c3df07ebac803e98af7e522086
sit23 <stephen.i.thomson@gmail.com>
2018-06-19 17:47:40 +0100

    Making project 3 longer to get closer to steady state.

--- experiments/earth_projects/project_3_storm_tracks/perp_equinox_aquaplanet.py
+++ experiments/earth_projects/project_3_storm_tracks/perp_equinox_aquaplanet.py
@@ -209,5 +209,5 @@ if __name__=="__main__":
         exp.set_resolution(*RESOLUTION)
 
         exp.run(1, use_restart=False, num_cores=NCORES)
-        for i in range(2,121):
+        for i in range(2,241):
             exp.run(i, num_cores=NCORES)
