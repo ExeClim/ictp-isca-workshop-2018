2c72b41469961f4661d85a642405ff40d024fcdd
sit23 <stephen.i.thomson@gmail.com>
2018-06-21 10:33:47 +0200

    Updating analyse single experiment to do seasonal analysis.

--- analysis/analyse_single_experiment.py
+++ analysis/analyse_single_experiment.py
@@ -3,9 +3,9 @@ import os
 import matplotlib.pyplot as plt
 import analyse_functions as af
 
-exp_folder_name = 'project_3_omega_normal'
+exp_folder_name = 'project_6_aquaplanet_obliq_23.439'
 start_file = 1
-end_file = 12
+end_file = 120
 
 file_name = 'atmos_monthly.nc'
 
@@ -21,6 +21,12 @@ dataset.t_surf_area_av.plot.line()
 af.global_average_lat_lon(dataset, 'temp')
 plt.figure()
 dataset.temp_area_av.transpose('pfull', 'time').plot.contourf()
+plt.ylim(dataset.pfull.max(), 0.)
+
+
+dataset.t_surf.groupby('seasons').mean('time').plot.contourf(col='seasons', col_wrap=2)
+
+dataset.ucomp.groupby('seasons').mean(('lon','time')).plot.contourf(col='seasons', col_wrap=2)
 plt.ylim(dataset.pfull.max(), 0.)
 
 plt.show()
