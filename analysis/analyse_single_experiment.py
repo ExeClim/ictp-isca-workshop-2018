ec2e7739503bea1f21adb49f1bf4304ac3e05360
sit23 <stephen.i.thomson@gmail.com>
2018-06-21 15:57:53 +0200

    Made analyse-single-experiment point to Frierson example script.

--- analysis/analyse_single_experiment.py
+++ analysis/analyse_single_experiment.py
@@ -3,29 +3,42 @@ import os
 import matplotlib.pyplot as plt
 import analyse_functions as af
 
-exp_folder_name = 'project_6_aquaplanet_obliq_23.439'
-start_file = 108
-end_file = 120
+#Folder name where data lies. Script will assume this data is in $GFDL_DATA directory
+exp_folder_name = 'frierson_grey_rad_example_experiment'
 
+#First month to be used
+start_file = 1
+#Final month to be used
+end_file = 24
+
+#Name of individual netcdf files to include. Example for monthly data.
 file_name = 'atmos_monthly.nc'
 
+#Open netcdf files as an xarray dataset object
 dataset = af.open_experiment(exp_folder_name, start_file, end_file, file_name)
 
+#Time and zonally average zonal wind and make contour plot
 dataset.ucomp.mean(('lon','time')).plot.contourf()
 plt.ylim(dataset.pfull.max(), 0.)
+plt.title('Time-averaged zonal-mean zonal wind')
 
+#Perform area mean of ssts and make line plot of time evolution
 af.global_average_lat_lon(dataset, 't_surf')
 plt.figure()
 dataset.t_surf_area_av.plot.line()
+plt.title('Time evolution of area-averaged sst')
 
+#Perform area mean of atmospheric temperatures and make contour plot
 af.global_average_lat_lon(dataset, 'temp')
 plt.figure()
 dataset.temp_area_av.transpose('pfull', 'time').plot.contourf()
 plt.ylim(dataset.pfull.max(), 0.)
+plt.title('Time and height evolution of area-mean atmospheric temperature')
 
-
+#Monthly-mean sst
 dataset.t_surf.groupby('months').mean('time').plot.contourf(col='months', col_wrap=4)
 
+#Seasonal mean zonal-mean zonal-wind
 dataset.ucomp.groupby('seasons').mean(('lon','time')).plot.contourf(col='seasons', col_wrap=2)
 plt.ylim(dataset.pfull.max(), 0.)
 
